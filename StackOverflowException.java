cd3977a7d4eef52ccfaccb5fdffc67cdc3842b55
Tom <tom.bailey@sonardyne.com>
2022-02-11 21:14:54 +0000

    commenting on stack overfow excception

--- StackOverflowException.java
+++ StackOverflowException.java
@@ -1,5 +1,11 @@
 public class StackOverflowException extends Exception {
 
+    /*
+    This is a simple custom exception which can 
+    be used when the message "Stack overflow." should
+    be printed
+    */
+
     public StackOverflowException()
     {
         super ("Stack overflow.");
